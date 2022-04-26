6f6acf09fbd24992120704b18e40393b36a90298
Alexis Guzman <alexis12989x@gmail.com>
2022-04-26 10:01:33 -0500

    feat: solve 8 problem

--- app.js
+++ app.js
@@ -73,3 +73,22 @@ const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) =>
 );
 
 // Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
+const assignBothFizzBuzzTrick = (explorer) => {
+  if (explorer.score % 5 === 0 && explorer.score % 3 === 0) {
+    explorer.trick = "FIZZBUZZ";
+    return explorer;
+  } else if (explorer.score % 5 === 0) {
+    explorer.trick = "BUZZ";
+    return explorer;
+  } else if (explorer.score % 3 === 0) {
+    explorer.trick = "FIZZ";
+    return explorer;
+  } else {
+    explorer.trick = explorer.score;
+    return explorer;
+  }
+};
+
+const explorersInNodeAndBothFizzBuzzTrick = explorersInNode.map((explorer) =>
+  assignBothFizzBuzzTrick(explorer)
+);
