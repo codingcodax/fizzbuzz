a2dfacf9adb938eee38554069fce733335a5f47b
Alexis Guzman <alexis12989x@gmail.com>
2022-04-26 12:39:34 -0500

    feat: use explorer service class

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const Reader = require("./lib/utils/Reader");
+const ExplorerService = require("./lib/services/ExplorerService");
 
 // Part 1 Read json file ===========================
 const explorers = Reader.readJsonFile("explorers.json");
 
 // Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter(
-  (explorer) => explorer.mission == "node"
-);
-//console.log(explorersInNode.length)
+const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+// console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"));
 
 // Part4: Get the explorer's usernames in Node
-const explorersInNodeToGetUsernames = explorers.filter(
-  (explorer) => explorer.mission == "node"
+const usernamesInNode = ExplorerService.getExplorersUsernmaesByMission(
+  explorers,
+  "node"
 );
-const usernamesInNode = explorersInNodeToGetUsernames.map(
-  (explorer) => explorer.githubUsername
-);
-//console.log(usernamesInNode)
+// console.log(usernamesInNode);
 
 // DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
